6203370f2661b6bcf39d999585cb09c418a813fe
Matheus Gonçalves Rodrigues <mgrbq@outlook.com>
2024-06-18 18:36:46 -0300

    Added value to properties in IdenityConfiguration class

--- DummyAuthentication/IdentityConfiguration.cs
+++ DummyAuthentication/IdentityConfiguration.cs
@@ -4,8 +4,39 @@ namespace DummyAuthentication;
 
 public class IdentityConfiguration
 {
-    public static IEnumerable<IdentityResource> IdentityResources { get; set; }
-    public static IEnumerable<ApiScope> ApiScopes { get; set; }
-    public static IEnumerable<ApiResource> ApiResources { get; set; }
-    public static IEnumerable<Client> Clients { get; set; }
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope("dummy-scope")
+        };
+
+    public static IEnumerable<ApiResource> ApiResources =>
+        new ApiResource[]
+        {
+            new ApiResource("dummy-resource")
+            {
+                Scopes = new List<string>{ "dummy-scope" }
+            }
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client()
+            {
+                ClientId = "dummy-consumer",
+                ClientName = "Random Consumer",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("d7mm1-5ecre7".Sha256()) },
+                AllowedScopes = { "dummy-scope" },
+                AccessTokenLifetime = 1200
+            }
+        };
 }
