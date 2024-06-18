065794f96a7533c9901098ba73ab9e1e68535fa5
Matheus Gonçalves Rodrigues <mgrbq@outlook.com>
2024-06-18 18:27:20 -0300

    configured identity in memory properties in Program

--- DummyAuthentication/Program.cs
+++ DummyAuthentication/Program.cs
@@ -1,6 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace DummyAuthentication;
 
-app.MapGet("/", () => "Hello World!");
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+        builder.Services
+            .AddIdentityServer()
+            .AddInMemoryClients(IdentityConfiguration.Clients)
+            .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+            .AddInMemoryApiResources(IdentityConfiguration.ApiResources)
+            .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+            .AddDeveloperSigningCredential();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+
+        app.UseIdentityServer();
+        app.UseRouting();
+
+        app.MapGet("/", () => "Authentication server running...");
+
+        app.Run();
+    }
+}
