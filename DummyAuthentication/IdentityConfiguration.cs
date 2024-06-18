2148dc45e7ac831215d68b14988a0662402cf5ea
Matheus Gonçalves Rodrigues <mgrbq@outlook.com>
2024-06-18 18:19:08 -0300

    Disabled nullable property for project and added properties to IdentityConfiguration class

--- DummyAuthentication/IdentityConfiguration.cs
+++ DummyAuthentication/IdentityConfiguration.cs
@@ -1,6 +1,11 @@
-﻿namespace DummyAuthentication;
+﻿using IdentityServer4.Models;
+
+namespace DummyAuthentication;
 
 public class IdentityConfiguration
 {
-
+    public static IEnumerable<IdentityResource> IdentityResources { get; set; }
+    public static IEnumerable<ApiScope> ApiScopes { get; set; }
+    public static IEnumerable<ApiResource> ApiResources { get; set; }
+    public static IEnumerable<Client> Clients { get; set; }
 }
